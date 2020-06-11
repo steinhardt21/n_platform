b6e5ef62495cc4437e9ee2e5867dd0f27e230b25
Alexandru Enache <44054368+steinhardt21@users.noreply.github.com>
2020-06-11 17:36:32 +0300

    first upload on github

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const Index = () => (
+
+    <Layout>
+        <div>
+            <h1>Welcome to Nucleus</h1>
+        </div>
+    </Layout>
+);
+
+export default Index;
